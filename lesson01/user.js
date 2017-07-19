567f9a5df57d09024891d3f0617a03f46b641b7e
web.master.month <web.master.month@gmail.com>
2017-07-19 08:36:07 +0300

    Пример работы с модулями формата json

--- lesson01/user.js
+++ lesson01/user.js
@@ -2,13 +2,15 @@
 // global - для глобальных переменных
 var x = 5;
 
+var phrases = require('./ru');
+
 function User(name) {
 	this.name = name;
 }
 
 User.prototype.hello = function (who){
 	//...
-	console.log("Hello, " + who.name);
+	console.log(phrases.Hello + ", " + who.name);
 };
 // ...
 
