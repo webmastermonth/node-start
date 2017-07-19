068cdf3921612ebf5f34d4ccf2f6173ab9001225
web.master.month <web.master.month@gmail.com>
2017-07-19 08:22:10 +0300

    Пример работы с require

---
+++ lesson01/user.js
@@ -0,0 +1,16 @@
+// Специальня переменная exports
+var x = 5;
+
+function User(name) {
+	this.name = name;
+}
+
+User.prototype.hello = function (who){
+	//...
+	console.log("Hello, " + who.name);
+};
+// ...
+
+console.log("user.js is required!");
+
+exports.User = User;
